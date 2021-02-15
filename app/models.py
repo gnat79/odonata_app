58eb3001e117a3e1c53e8df2270283cc6a5e3baf
Nathaniel Schwartz <gnat79@primebook.local>
2021-02-14 21:20:57 -0500

    added species and image models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -8,3 +9,27 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Species(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    binomial = db.Column(db.String(256), index=True, unique=True)
+    description = db.Column(db.Text)
+    identification = db.Column(db.Text)
+    last_modified = db.Column(db.DateTime, default=datetime.utcnow)
+    images = db.relationship('Image', backref='species', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Species {}>'.format(self.binomial)
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file = db.Column(db.String(256))
+    species_id = db.Column(db.Integer, db.ForeignKey('species.id'))
+    last_modified = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Image {}>'.format(self.species_id)
+
+
+# Column(ARRAY(TEXT)) -- store a bunch of versions of the description
+
